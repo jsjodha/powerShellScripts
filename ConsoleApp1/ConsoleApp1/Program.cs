892d1fd95c645f1af4238b392add7175bfe900fb
Jhoonjhar <jhoonjhar@gmail.com>
2019-07-16 18:50:14 +0530

    added Process lister program.

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var procs = System.Diagnostics.Process.GetProcesses();
+
+            foreach (Process item in procs)
+            {
+                Console.WriteLine(item.ProcessName);
+            }
+        }
+    }
+}
